ac7aa6e43432e53feba81808775e1dc19d945a07
AADIL KHALIL BAIG <20002369@myrp.edu.sg>
2021-05-06 16:08:10 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20002369/ca1practical2/MainActivity.java
@@ -0,0 +1,24 @@
+package sg.edu.rp.c346.id20002369.ca1practical2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView display;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        display = findViewById(R.id.textViewDisplay);
+
+    }
+}
